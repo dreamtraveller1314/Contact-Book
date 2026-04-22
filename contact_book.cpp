70b3d5a6af1cf2fbffb911f5ae295d3c7fa94214
dreamtraveller1314 <xinru2509@gmail.com>
2026-04-22 09:54:17 +0800

    new entry

--- contact_book.cpp
+++ contact_book.cpp
@@ -14,3 +14,28 @@ typedef struct Contact {
 
 char *FILENAME = "contact_book_data.csv";
 Contact contact_array[100];
+
+int add_new_entry() {
+    FILE *fptr = fopen(FILENAME, "a");
+    if (fptr == NULL) return 1;
+    char name[INPUT_LENGTH], address[INPUT_LENGTH], phone[INPUT_LENGTH], email[INPUT_LENGTH];
+    
+    printf("Enter name: ");
+    fgets(name, INPUT_LENGTH, stdin);
+    name[strcspn(name, "\n")] = 0;
+    printf("Enter address: ");
+    fgets(address, INPUT_LENGTH, stdin);
+    address[strcspn(address, "\n")] = 0;
+    printf("Enter phone: ");
+    fgets(phone, INPUT_LENGTH, stdin);
+    phone[strcspn(phone, "\n")] = 0;
+    printf("Enter email: ");
+    fgets(email, INPUT_LENGTH, stdin);
+    email[strcspn(email, "\n")] = 0;
+
+    fprintf(fptr, "%s,%s,%s,%s\n", name, address, phone, email);
+    fclose(fptr);
+
+    printf("Contact added successfully!\n");
+    return 0;
+}
